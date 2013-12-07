11b2c2d652ae88422c19742a0c55653e5e9e9d5d
rachelkelly <rachelkelly@users.noreply.github.com>
2013-12-07 15:38:29 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+#ha ha!
+
+print "how much dollaz you got"
+oldMoney = float(raw_input())
+print "how much dollaz you get"
+newMoney = float(raw_input())
+currentMoney = oldMoney + newMoney
+print "so you're adding %s to your old monay, %s, which is %s." % (newMoney, oldMoney, currentMoney)
+
+print "since I'm fabulous and brilliant, I'll go ahead and subdivide your new dollaz."
+
+subdividable = newMoney
+#all these categories will have to have another file to refer to
+bills = bills + 1650
+subdividable =- bills
+
+print "after bills (rent, electricity, water, comcast), you have %s left." % subdividable
+print "you have %s in the billz envelope" % bills
+
+shoes = shoes + 20
+subdividable =- shoes
+
+print "after adding to shoe fund you have %s left" %subdividable
+print "you got hella hella %s for shoez" % shoes
+
+vacay =+ 150
+subdividable =- vacay
+
+print "after adding to vacation fund you have %s left" % subdividable
+print "you have $%s left for hella boozin' in the cocomos" % vacay
+
+clothes =+ 150
+subdividable =- clothes
+
+print "left: %s" % subdividable
+print "clothesfund: %s" % clothes
