6a9d6caca3ea5939cf8c70c07269e72b3283d491
rachelkelly <rachelkelly@users.noreply.github.com>
2014-02-26 17:14:02 -0800

    always want to fiddle after I do a major commit!
    fiddled a LOT.  mostly added notes & created a denser sorta design document.

--- main.py
+++ main.py
@@ -20,18 +20,22 @@ if newMoney > 2500:
 else:
 	pass
 if newMoney > 1000:
-	print "let's divide this evenly between vacation & savings."
+	print "let's divide this evenly between vacation, savings & clothes."
+	vacationEnvelope += (newMoney/3)
+	savingsEnvelope += (newMoney/3)
+	clothesEnvelope += (newMoney/3)
+	summary()
 else:
 	print "extra?  we'll put it in savings."
 	savings += newMoney
 	print "okay, now you have %s in savings." % savings
 
-def subdivision:
+def subdivision(newMoney):
 	subdividable = newMoney
 	print "you have %s to break up here." % subdividable
 
 	for i in expenses:
-		print "putting \$ %s toward %s envelope." % float(i), i
+		print "putting \$ %s toward %s envelope." % (float(i), i)
 		#I don't think this will work to declare the number
 		#relevant to each member of the list of expenses
 		#ideally above print stmt says, at least for the rent portion:
@@ -40,6 +44,21 @@ def subdivision:
 		subdividable -= i
 		#and now how to put actual amount toward each i?
 		#rentenvelope += float(i)
+		
+		print "there is %s in the envelope belonging to %s now" % (float(i), i)
+	
+	print "now you've got %s left.  that's all gonna go into THE FUTURE." % subdividable
+	theFutureEnvelope += subdividable
+	
+	assert subdividable == 0
 
 # basically I want to do the subdivision portion in one big for loop for the list of expenses,
 # iterating through correctly & then stopping after one round.
+
+
+def summary():
+	#want to print all envelope amounts here
+	#want to print remaining (estimated?) balance on loans.
+		# would probably have to import math to get e in order to do the P*e etc equation
+	#also probably a good idea to list when all bills get paid.
+	command = 
