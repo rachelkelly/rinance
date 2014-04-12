6ccf6591fbbd138f49a7f3551dc2a0ebef7a87b0
Rachel Kelly <rachelkelly@gmail.com>
2014-04-12 14:05:05 -0700

    tested the first chunkletgit add main.py  YEAH TESTING

--- main.py
+++ main.py
@@ -10,14 +10,27 @@
 #   print things-to-pay
 #
 #then "what is incoming amount"
+
+#below line for testing purposes
+#static_rent = static_sprint = static_oil = 0
+
+amount = 1550
 if amount > 1500:
-	#maybe my arithm is dumb
-	in_rent = amount-(amount-525)
-	now take it away from the amount:
-	amount = amount - in_rent
-	static_rent = static_rent + in_rent
-	in_sprint = amount-(amount-200)
-	
+    in_rent = amount-(amount-525)
+    static_rent = static_rent + in_rent
+    amount = amount - in_rent
+
+    in_sprint = amount-(amount-200)
+    static_sprint = static_sprint - in_sprint
+    amount = amount - in_sprint
+
+    in_oil = amount - (amount-50)
+    static_oil = static_oil - in_oil
+    amount = amount - in_oil
+    
+    
+
+    print amount
 #	
 #       
 #	
