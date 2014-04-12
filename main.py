6638a25134064f04a46d0253758dc21bc30c21aa
Rachel Kelly <rachelkelly@gmail.com>
2014-04-12 13:45:24 -0700

    ok, starting over.

---
+++ main.py
@@ -0,0 +1,24 @@
+# 2014 rachel kelly, creative commons license
+# a program for budgeting
+
+#some pseudocode:
+#open moneyfile read/write
+#envelopes = {}
+#for line in moneyfile:
+#   make dict, keys are things-to-pay & values are $$ designated
+#   each thing-to-pay
+#   print things-to-pay
+#
+#then "what is incoming amount"
+if amount > 1500:
+	#maybe my arithm is dumb
+	in_rent = amount-(amount-525)
+	now take it away from the amount:
+	amount = amount - in_rent
+	static_rent = static_rent + in_rent
+	in_sprint = amount-(amount-200)
+	
+#	
+#       
+#	
+# please enter $ currently in BOFA:
