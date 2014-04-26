9bdcf837b17a323865a3ceb3d69baa1aaf5787b7
Rachel Kelly <rachelkelly@gmail.com>
2014-04-26 12:09:33 -0700

    started on the bill_pay function.  LOTS wrong.

--- main.py
+++ main.py
@@ -5,9 +5,9 @@
 # turns out there are lots of ways to approach this problem.
 
 # program map
-# first, in_or_out determines whether check deposit(in) or billpay(out)
+# first, in_or_out() determines whether check deposit(in) or billpay(out)
 # in: 
-# a function called money_distribution() determines, based on
+# a function called envelope_distribution() determines, based on
 # deposit, if it will be a total distribution or a partial one,
 # "partial" being that the user will decide how much & where the
 # amounts will be distributed.
@@ -16,42 +16,65 @@
 # will automatically be deducted from their respective envelopes, or
 # b) abnormal and the user will indicate which bill is being paid & how much.
 
+normal_bills = {'normal_sprint_out': 160, 'normal_oil_out': 50, 'normal_rent_out': 1050,
+               'normal_comcast_out': 75, 'normal_loans_out': 450, 'vacation': 50}
+print normal_bills
+inexact_bills = ["electric", "savings", "groceries", "food out", "clothes", "vacation"]
+print inexact_bills
 def getfile():
     moneyfile = open("envelopes.txt", "r")
     moneyitems = {}
+
     for line in moneyfile:
         entry = line.strip().split(",")
         lineitem = entry[0]
         amount = entry[1]
         moneyitems[lineitem] = amount
         print "current %s amount: $%r" % (lineitem, amount)
+    print moneyitems
+    moneyfile.close()
 
 def in_or_out():
-    print "money in or out?"
-    in_out_choice = raw_input("> ")
+    print "hi!  money in or out?"
+    in_out_choice = raw_input("> ")    
     if in_out_choice == "in":
-        #have to make this var
-        money_distribution(in_out_choice)
+        envelope_distribution(in_out_choice)    
     elif in_out_choice == "out":
         #have to still make this var
-        billpay(in_out_choice)
+        bill_pay(in_out_choice)
     elif in_out_choice == "quit":
         exit(0)
     else:
         print "no doof, type 'in' or 'out', or type 'quit' to, yknow."
         in_or_out()
 
-def money_distribution(in_out_choice):
-    print "success 1"    
+def envelope_distribution(in_out_choice):
+    pass
 
-def billpay(in_out_choice):
-    print "success 2"
+# this has a long way to go
+def bill_pay(in_out_choice):
+    print "which bill are you paying?"
+    whichbill = raw_input("> ")
+    if whichbill in moneyitems:
+#        print "the %s is usually $%d.  is that how much it is this time?  y/n" % (whichbill, [*])
+        #above line: e.g. 'the sprint bill is usually $175.  is that...'
+        print "you can also quit at this point."
+        typical_choice = raw_input("> ")
+        if typical_choice == "y":
+            print "y"
+
+        elif typical_choice == "n":
+            print "n"
+        elif typical_choice == "quit":
+            exit(0)
+        else:
+            bill_pay()
 
 #test harness attempt
 #it works but is repetitive for me - but that's ok maybe!
 #"testing turns fear into boredom"
 #for i in range (10):
-#    in_or_out()
+    #in_or_out()
 
 #in_or_out()
-#getfile()
+getfile()
