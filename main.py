9fee6e1bde7fba9f3d707f9873616447fd562605
Rachel Kelly <rkellyalso@gmail.com>
2014-05-25 22:50:37 -0700

    enhanced program map.  made dicts global.

--- main.py
+++ main.py
@@ -4,17 +4,29 @@
 # this is a third attempt at this program!
 # turns out there are lots of ways to approach this problem.
 
-# program map
-# first, in_or_out() determines whether check deposit(in) or billpay(out)
-# in: 
-# a function called envelope_distribution() determines, based on
-# deposit, if it will be a total distribution or a partial one,
-# "partial" being that the user will decide how much & where the
-# amounts will be distributed.
+# first, in_or_out() determines whether payday(in) or bills(out)
+# 
+# in:
+# fn envelope_distribution() determines payday distribution.  if amt > 1000,
+# then 550 to rent, 75 to sprint, 50 to electric, 40 to comcast, 50 to geico,
+# 50 to water, 200 to food, rest to savings
+# if amt < 1000, 300 to loans, rest to savings
+# all contributions go toward the item in moneyitems.  not sure how this
+# will work just yet.  it will add to static_$ENVELOPE and write it to
+# envelopes.txt file.
+#
 # out:
-# a function called billpay() will first ask if a) normal, so all EOM bills
-# will automatically be deducted from their respective envelopes, or
-# b) abnormal and the user will indicate which bill is being paid & how much.
+# fn billpay() will print all items in moneyfile into a dict called
+# moneyitems.  moneyitems is global so any function can use it.
+# then the user will indicate which bill she is to pay.  then if the input
+# has a match in {moneyitems}, it will ask "is it a normal payout," and if it
+# is, it will take that amount out.
+# e.g. "is 'static_sprint' a normal payout this month," "yes," "ok, taking
+# $160 out of 'static_sprint' envelope."
+# after a bill is paid, it will ask the user if she wants to pay another.
+# if so, then the process repeats identically.
+# if not, the program prints all bills paid (so a new dict is made of paid
+# bills to report at end of program), all envelope quantities.
 
 normal_bills = {'normal_sprint_out': 160, 'normal_oil_out': 50, 
                 'normal_rent_out': 1050, 'normal_comcast_out': 75,
